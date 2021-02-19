a458454561e930218d85317fd0d6b2bb985713f4
yashasvi sakure <yashasvisakure@gmail.com>
2021-02-19 15:27:46 +0530

    add ysme to the people

---
+++ people/ysme.java
@@ -0,0 +1,16 @@
+class ysme{
+    public static void main(String[] args){
+        System.out.println(" 
+        module.exports = {
+                name: 'YS_ME',
+                github: 'https://github.com/Edith-panda/',
+                email: 'thisisme06@xxx.com',
+                twitter: '@ysme',
+                facebook: 'ysme',
+                linkedin: 'in/ysme...'
+              }");
+
+    }
+}
+
+
